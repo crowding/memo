dbb360972cbcdc4cd9978531e02e09cfb6ac3602
Peter Meilstrup <peter.meilstrup@gmail.com>
2016-08-23 16:57:02 -0700

    Way of representing floats for Windows

--- src/getPointer.c
+++ src/getPointer.c
@@ -2,12 +2,38 @@
 
 SEXP stringify_item(SEXP, char *);
 SEXP _string_reps(SEXP);
+int sprintdouble(char *, double);
+
+#if defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
+int sprintdouble(char *buf, double arg) {
+  return sprintf(buf, "%la", arg);
+}
+#else
+/* hack for stdlib that doesn't support "%a" printf conversion */
+int sprintdouble(char *buf, double arg) {
+  int written = 0;
+  union {
+    double f;
+    unsigned char ch[sizeof(double) / sizeof(char)];
+  } data;
+  data.f = arg;
+
+  for (int i = 0; i < sizeof(double) / sizeof(char); i++) {
+    int chars = sprintf(buf, "%02x", data.ch[i]);
+    written += chars;
+    buf += chars;
+  }
+  return written;
+}
+#endif
+
+
 
 /* Return some canonical identifying strings for each of a list of objects.
- * For scalars, the values are directly represented.
+ * For numeric scalars, the values are directly represented.
  * For scalar strings, we use the pointer to the interned CHARSXP.
- * For other objects, we use the pointer.
- * If the list has names, we represent store STRSXP pointers to the names
+ * For other objects, we use the pointer, after setting NAMED=2.
+ * If the list has names, we represent the pointer values on the names.
  * Because it is possible for objects-pointed-to to be GCed and replaced with 
  * different objects, the calling code is responsible for holding on to
  * references to the objects (see test-cache.R).
@@ -34,8 +60,9 @@ SEXP _string_reps(SEXP list) {
 
   for (int i = 0; i < length; i++) {
     SEXP item = VECTOR_ELT(list, i);
-    char buf[99]; /* worst case, element has a name and is a CLOSXP;
-                     "c01234567=c_01234567/01234567/01234567/01234567" */
+    /* worst case, element has a tag and is a CLOSXP, so 4 pointers and change:
+       "c0x0123456789abcdef=c_0x0123456789abcdef/0x0123456789abcdef/0x01234567abcdef/0x01234567abcdef" = 97 chars*/
+    char buf[128];
     char *bufptr = buf;
 
     if (in_names != R_NilValue) {
@@ -58,27 +85,30 @@ SEXP _string_reps(SEXP list) {
   return(out_reps);
 }
 
-/* Construct a string identifying some SEXP, either as a scalar value or as a pointer. */
+/* Construct a string identifying some SEXP, either as a scalar value or as a pointer.
+   If we use its pointer, set NAMED = 2 on the pointer used.
+   Return that pointer, or R_NilValue. */
 SEXP stringify_item(SEXP item, char *bufptr) {
   int done = 0;
-  SEXP result = R_NilValue;
+  SEXP item_ptr = R_NilValue;
   while(!done) {
     switch (TYPEOF(item)) {
     case PROMSXP:
       /* if we have a promise, drill down. */
-      item=PRCODE(item);
+      item = PRCODE(item);
       break;
     case CHARSXP:
       /* interned string, represent its pointer */
-      bufptr += sprintf(bufptr, "c%p", CHAR(item)); break;
-      result = item;
+      item_ptr = item;
+      bufptr += sprintf(bufptr, "c%p", CHAR(item_ptr));
+      done = 1;
+      break;
     case REALSXP:
     case INTSXP:
     case STRSXP:
     case LGLSXP:
       /* we have a code literal. represent it canonically,
          and don't hold a ref to a scalar. */
-      result = R_NilValue;
       if (LENGTH(item) == 0) {
         switch(TYPEOF(item)) {
         case REALSXP: bufptr += sprintf(bufptr, "r0"); break;
@@ -89,32 +119,35 @@ SEXP stringify_item(SEXP item, char *bufptr) {
         }
       } else if (LENGTH(item) == 1) {
         switch(TYPEOF(item)) {
-        case REALSXP: bufptr += sprintf(bufptr, "r%la", REAL(item)[0]); break;
+        case REALSXP:
+          bufptr += sprintf(bufptr, "r");
+          bufptr += sprintdouble(bufptr, REAL(item)[0]);
+          break;
         case INTSXP: bufptr += sprintf(bufptr, "i%x", INTEGER(item)[0]); break;
         case LGLSXP: bufptr += sprintf(bufptr, "l%x", LOGICAL(item)[0]); break;
         case STRSXP:
-          bufptr += sprintf(bufptr, "s%p", CHAR(STRING_ELT(item, 0))); break;
-          result = STRING_ELT(item, 0);
+          item_ptr = STRING_ELT(item, 0);
+          bufptr += sprintf(bufptr, "s%p", CHAR(item_ptr)); break;
         default: error("Unexpected type %s (this shouldn't happen)", TYPEOF(item));
         }
       } else {
-        /* for longer values, represent the pointer */
-        bufptr += sprintf(bufptr, "v%p", (void *)item);
-        result = item;
+        /* for non-scalar vectors, represent the pointer */
+        item_ptr = item;
+        bufptr += sprintf(bufptr, "v%p", (void *)item_ptr);
       }
       done = 1;
       break;
     case VECSXP:
-      bufptr += sprintf(bufptr, "l%p", (void *)item);
-      result = item;
+      item_ptr = item;
+      bufptr += sprintf(bufptr, "l%p", (void *)item_ptr);
       done = 1;
       break;
     case CLOSXP:
+      item_ptr = item;
       bufptr += sprintf(bufptr, "c_%p/%p/%p",
                         (void *) FORMALS(item),
                         (void *) BODY(item),
                         (void *) CLOENV(item));
-      result=item;
       done = 1;
       break;
     case SYMSXP:
@@ -125,16 +158,18 @@ SEXP stringify_item(SEXP item, char *bufptr) {
     case SPECIALSXP:
     case NILSXP:
       /* We have an expression-ish, represent its pointer. */
-      bufptr += sprintf(bufptr, "e%p", (void *)item);
-      result = item;
+      item_ptr = item;
+      bufptr += sprintf(bufptr, "e%p", (void *)item_ptr);
       done = 1;
       break;
     default:
       error("Unexpected type %s", type2char(TYPEOF(item)));
     }
   }
-  SET_NAMED(result, 2);
-  return result;
+  if (item_ptr != R_NilValue) {
+    SET_NAMED(item_ptr, 2);
+  }
+  return item_ptr;
 }
 
 /*
